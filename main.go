02b5933388a1215021c0c0df9f03ca83178d8de4
Yurino Oga <yurino.o@latona.io>
2022-04-28 15:36:58 +0900

    release

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	sap_api_caller "sap-api-integrations-employment-information-reads/SAP_API_Caller"
-	"sap-api-integrations-employment-information-reads/sap_api_input_reader"
+	sap_api_input_reader "sap-api-integrations-employment-information-reads/SAP_API_Input_Reader"
 
 	"github.com/latonaio/golang-logging-library/logger"
 )
